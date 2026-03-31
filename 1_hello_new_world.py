21f286475a5404c437d58602910c9204b1ebdef2
Suxumuxu <emmanouil.xylakis@gmail.com>
2026-03-31 18:03:44 +0300

    python lesson 1

---
+++ 1_hello_new_world.py
@@ -0,0 +1,46 @@
+print("Hello my New World in Python")
+
+name = "Manolis"
+height = 1.75
+age = 38 
+teaching_now = True 
+
+x = 7
+y = 8 
+
+print(str(x)+str(y))
+print(x+y)
+print(name + " is " + str(height) + " and is " + str(age) + " years old. " + "Is he teaching now?: " + str(teaching_now))
+print(f"{name} is {height} and is {age} years old. Is he teaching now?: {teaching_now}")
+
+print(type(age))
+print(type(teaching_now))
+
+# name = input("What is your name? ")
+# height = input("And what is your height? ") 
+# age = input("How old are you, if I may? ")
+# action = input("and what are you dooing now? ")
+
+# print(f"{name} is {height} and is {age} years old. And he is: {action} ")
+
+# print(name + " is " + height + " and is " + age + " years old. And he is: " + action )
+
+# print("Height is of type: ", type(height))
+# print("Age is of type: ", type(age))
+
+# height = float(height)
+# age = int(age)
+
+# print("Height is of type: ", type(height))
+# print("Age is of type: ", type(age))
+
+apples = "Mitsos"
+
+print(apples)
+
+# apples = float(apples)
+apples = float(apples)
+
+print(apples)
+
+
