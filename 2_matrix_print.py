9aadff45f7498ce172b3bbcfa97f67c277d2dcf6
Suxumuxu <emmanouil.xylakis@gmail.com>
2026-04-23 18:40:28 +0300

    Update matrix

--- 2_matrix_print.py
+++ 2_matrix_print.py
@@ -1,11 +1,14 @@
 matrix = [[j for j in range(i, i + 6)] for i in range(1, 31, 6)]
 
 my_matrix = [
+    
     [1,2,3,4,5,6],
     [7,8,9,10,11,12],
     [13,14,15,16,17,18],
     [19,20,21,22,23,24],
-    [25,26,27,28,29,30]]
+    [25,26,27,28,29,30]
+    
+    ]
 
 
 print("To stoixeio sth grammi 2 kai sth stili 4 einai to :" +str(matrix[2][4])) 
